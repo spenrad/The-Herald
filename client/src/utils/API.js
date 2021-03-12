063b2ed21b750844868d11aa1969e352e0685a9e
Spencer Christy <spenrad@gmail.com>
2021-03-11 22:23:05 -0800

    successfully moved axios get of newapi from front to back end, had to change the client request to a post, which made req.body available to pass off to the post request to the server which executes a get request from the url which now has the parameters available to in within req.body

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,46 +1,52 @@
 import axios from "axios";
 
 export default {
-    newsArticles: function(search, date) {
-        return axios.get("/api/newsArticles", search, date)
-    },
+  newsArticles: function (search, date) {
+    return axios
+      .post("/api/newsArticles", {search, date})
+  },
 
-    newsArticlesProfile: function(search) {
-        return axios.get("https://newsapi.org/v2/everything?qInTitle=" + search +"&language=en&sortBy=popularity&apiKey=0e220e65f7324225a3a82a01eef04123")
-    },
+  newsArticlesProfile: function (search, date) {
+    return axios.get(
+      "https://newsapi.org/v2/everything?qInTitle=" +
+        search +
+        date +
+        "&language=en&sortBy=popularity&apiKey=0e220e65f7324225a3a82a01eef04123"
+    );
+  },
 
-    // NOTE: axios routes are for development only, add a ternary statement or update once in production
-    registerUser : function (info) {
-        return axios.post("/api/signup", info)
-    },
+  // NOTE: axios routes are for development only, add a ternary statement or update once in production
+  registerUser: function (info) {
+    return axios.post("/api/signup", info);
+  },
 
-    loginUser: function (info) {
-        return axios.post("/api/login", info, {
-            withCredentials: true
-          })
-    },
+  loginUser: function (info) {
+    return axios.post("/api/login", info, {
+      withCredentials: true,
+    });
+  },
 
-    updateTopic : function (info) {
-        return axios.put("/api/update", info, {
-            withCredentials: true
-          })
-    },
+  updateTopic: function (info) {
+    return axios.put("/api/update", info, {
+      withCredentials: true,
+    });
+  },
 
-    authUser: function () {
-        return axios.get("/api/checkAuthentication", {
-            withCredentials: true
-          })
-    },
+  authUser: function () {
+    return axios.get("/api/checkAuthentication", {
+      withCredentials: true,
+    });
+  },
 
-    getUserInfo: function () {
-        return axios.get("/api/userTopics", {
-            withCredentials: true
-          } )
-    },
+  getUserInfo: function () {
+    return axios.get("/api/userTopics", {
+      withCredentials: true,
+    });
+  },
 
-    logoutUser: function () {
-        return axios.get("/logout", {
-            withCredentials: true
-          } )
-    }
-}
+  logoutUser: function () {
+    return axios.get("/logout", {
+      withCredentials: true,
+    });
+  },
+};
--- routes/api/newsArticles.js
+++ routes/api/newsArticles.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const axios = require("axios");
 
-router
-.get('/', function (req, res) {
-     axios.get("https://newsapi.org/v2/everything?qInTitle=" + search + date + "&language=en&sortBy=popularity&apiKey=df5325c355274b608c24ce203beb33ce").then( (info)=> {
-      console.log("articles", info.data);
-      let articles =info.data
-      res.json(articles)
-      })
-    })
-    
+router.post("/", function (req, res) {
+    axios
+      .get("https://newsapi.org/v2/everything?qInTitle=" +req.body.search + req.body.date + "&language=en&sortBy=popularity&apiKey=df5325c355274b608c24ce203beb33ce"
+      )
+      .then((info) => {
+        console.log("articles", info.data);
+        let articles = info.data;
+        res.json(articles);
+      });
+});
 
 module.exports = router;
